3e94df3ea770b75fb776a6c78fcd7cf11bf02867
tranphukhang <tranphukhang2003@gmail.com>
2026-08-11 15:17:18 +0700

    Revert "Update"
    This reverts commit f4d7ec7a275d770e3de5c73add066dfc69005a6f.

--- run.py
+++ run.py
@@ -729,8 +729,6 @@ def main():
           target_pos = ctrl.step()
         ctrl.apply_pd_control(target_pos)
 
-        mujoco.mj_step(model, data)
-
         # ============================================================
         # TEST_TIMING_ONLY BEGIN
         # Kiểm tra giả thuyết:
